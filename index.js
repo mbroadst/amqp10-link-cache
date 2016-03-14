db812f2f74eec38c7d58d7233f5f21a2fee421fa
Matt Broadstone <mbroadst@gmail.com>
2016-03-14 17:09:03 -0400

    feat(purging): support purging unused links from link cache

--- index.js
+++ index.js
@@ -2,35 +2,76 @@
 var Promise = require('bluebird'),
     hash = require('object-hash');
 
-function LinkCache(client) {
+function LinkCache(client, options) {
+  options = options || {};
   this._client = client;
-  this._receiverLinks = {};
-  this._senderLinks = {};
+  this._links = {};
+  this._ttl = options.ttl || 60000;
+  this._purgeTimeout = null;
+
+  client.on('disconnected', function() {});
 }
 
 LinkCache.prototype.createSender = function(address, options) {
-  return this._createLink(address, options, this._senderLinks, 'createSender');
+  return this._createLink(address, options, 'sender', 'createSender');
 };
 
 LinkCache.prototype.createReceiver = function(address, options) {
-  return this._createLink(address, options, this._receiverLinks, 'createReceiver');
+  return this._createLink(address, options, 'receiver', 'createReceiver');
 };
 
-LinkCache.prototype._createLink = function(address, options, container, method) {
-  var linkHash = hash({ address: address, options: options });
-  if (container.hasOwnProperty(linkHash)) {
-    return Promise.resolve(container[linkHash]);
+LinkCache.prototype._createLink = function(address, options, type, method) {
+  var linkHash = hash({ type: type, address: address, options: options });
+  if (this._links.hasOwnProperty(linkHash)) {
+    var entry = this._links[linkHash];
+    if (entry instanceof Promise)
+      return entry;
+
+    this._links[linkHash].stamp = Date.now();
+    return Promise.resolve(this._links[linkHash].link);
   }
 
+  var self = this;
   var linkPromise = this._client[method](address, options)
     .then(function(link) {
-      link.once('detached', function() { delete container[linkHash]; });
-      container[linkHash] = link;
+      link.once('detached', function() {
+        if (self._links.hasOwnProperty(linkHash))
+          delete self._links[linkHash];
+      });
+
+      self._links[linkHash] = { link: link, stamp: Date.now() };
+      if (!self._purgeTimeout)
+        self._purgeTimeout = setTimeout(self._purgeLinks.bind(self), self._ttl);
       return link;
     });
 
-  container[linkHash] = linkPromise;
+
+  this._links[linkHash] = linkPromise;
   return linkPromise;
+};
+
+LinkCache.prototype._purgeLinks = function() {
+  var now = Date.now();
+  var _keys = Object.keys(this._links),
+      expired = [], live = 0;
+
+  for (var i = 0, ii = _keys.length; i < ii; ++i) {
+    if (now - this._links[_keys[i]].stamp >= this._ttl) {
+      expired.push(_keys[i]);
+    } else {
+      live++;
+    }
+  }
+
+  for (var j = 0, jj = expired.length; j < jj; ++j) {
+    var cacheEntry = this._links[expired[j]];
+    delete this._links[_keys[j]];
+    cacheEntry.link.detach();
+  }
+
+  if (live && !this._purgeTimeout) {
+    this._purgeTimeout = setTimeout(this._purgeLinks.bind(this), this._ttl);
+  }
 };
 
 module.exports = LinkCache;
--- test/link-cache.test.js
+++ test/link-cache.test.js
@@ -60,4 +60,28 @@ describe('LinkCache', function() {
     });
   });
 
+  it('should purge links after a given interval', function() {
+    test.cache = new LinkCache(test.client, { ttl: 1 });
+    return test.client.connect(config.address)
+      .then(function() { return test.cache.createSender('amq.topic'); })
+      .delay(5)
+      .then(function() {
+        var linkCount = Object.keys(test.cache._links).length;
+        expect(linkCount).to.equal(0);
+      });
+  });
+
+  it('should purge links after a given interval (2)', function() {
+    test.cache = new LinkCache(test.client, { ttl: 1 });
+
+    var sender1, sender2;
+    return test.client.connect(config.address)
+      .then(function() { return test.cache.createSender('amq.topic'); })
+      .tap(function(sender) { sender1 = sender; })
+      .delay(5)
+      .then(function() { return test.cache.createSender('amq.topic'); })
+      .tap(function(sender) { sender2 = sender; })
+      .then(function() { expect(sender1).to.not.eql(sender2); });
+  });
+
 });
