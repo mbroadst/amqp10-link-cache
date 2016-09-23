65ba3cda2f045e794c3b0d53ae3fcc8e8bf09d00
Matt Broadstone <mbroadst@gmail.com>
2016-09-23 08:35:43 -0400

    test(cache): add test to ensure caches are made per-client

--- test/link-cache.test.js
+++ test/link-cache.test.js
@@ -83,4 +83,17 @@ describe('basic behavior', function() {
         expect(link1).to.not.eql(link2);
       });
   });
+
+  it('should maintain link caches per-client', function() {
+    test.client2 = new AMQPClient();
+    return Promise.all([ test.client.connect(config.address), test.client2.connect(config.address) ])
+      .then(function() {
+        return Promise.all([
+          test.client.createReceiver('amq.topic'),
+          test.client2.createReceiver('amq.topic')
+        ]);
+      })
+      .spread(function(link1, link2) { expect(link1).to.not.eql(link2); })
+      .then(function() { delete test.client2; });
+  });
 });
